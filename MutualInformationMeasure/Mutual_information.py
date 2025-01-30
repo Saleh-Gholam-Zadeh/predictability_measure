931ca215f0015600aa2a8c6b9b5748740760661e
Saleh-Gholam-Zadeh <salehgholamzadeh17@gmail.com>
2025-01-30 17:04:50 +0100

    vectorized implementation of MI

--- MutualInformationMeasure/Mutual_information.py
+++ MutualInformationMeasure/Mutual_information.py
@@ -12,28 +12,30 @@ def permute_1d_array(arr):
     permuted_arr = np.random.permutation(arr)
     return permuted_arr
 
-def get_mutual_information(data, number_output_functions=1, min_n_datapoints_a_bin = None, perm_test_flag=True, N=10):
-
+def compute_normalized_mutual_information_np(freq_data_product, expfreq, basis_log_mi):
     """
-        data: a 2D numpy array in which the first "number_output_functions" rows are the values of the output function (or target values) and the other rows are the values of the features (input or context)
-        Implementation of MI code
-        samples : each sample stored in one column
-        n_col---> number of samples
-        n_rows ---> number of output + features (input)
-        i.e
-        m = data.shape[0]  # number features (plus targets)
-        n = data.shape[1]  # number of data points
-        the whole length of target + context = m ---> target comes first
-        each column of the data: [t1, t2, ..., t_num_output_function, ctx1, ctx2, ... m ]
-        number_output_functions: how many outputs we have
+    Compute normalized mutual information between freq_data_product and expfreq,
+    normalized by log2(basis_log_mi), in a vectorized way.
+    Parameters:
+        freq_data_product (np.ndarray): A 2D NumPy array representing observed frequencies.
+        expfreq (np.ndarray): A 2D NumPy array representing expected frequencies.
+        basis_log_mi (int): Basis for log normalization.
+    Returns:
+        float: Normalized mutual information.
     """
-    # Calulate the Shannon mutual information
-    def make_summand_from_frequencies(x, y):
-        if x == 0:
-            return 0
-        else:
-            return x * math.log2(x / y) / math.log2(basis_log_mutual_information)
+    # Ensure inputs are NumPy arrays
+    freq_data_product = np.asarray(freq_data_product)
+    expfreq = np.asarray(expfreq)
+    # Mask where freq_data_product is non-zero to avoid division or log issues
+    mask = freq_data_product != 0
+    # Compute the log term and normalize it
+    log_term = np.log2(freq_data_product[mask] / expfreq[mask])
+    normalized_log_term = log_term / np.log2(basis_log_mi)
+    # Compute the weighted sum for mutual information
+    mutual_info = np.sum(freq_data_product[mask] * normalized_log_term)
+    return mutual_info
 
+def get_mutual_information(data, number_output_functions=1, min_n_datapoints_a_bin=None, perm_test_flag=True, N=10):
 
     m = data.shape[0]
     n = data.shape[1]
@@ -44,8 +46,7 @@ def get_mutual_information(data, number_output_functions=1, min_n_datapoints_a_b
     data = data[list_variables, :]
 
     if (min_n_datapoints_a_bin is None):
-        min_n_datapoints_a_bin = 0.05*n
-
+        min_n_datapoints_a_bin = 0.05 * n
 
     constant_features = []
 
@@ -67,8 +68,9 @@ def get_mutual_information(data, number_output_functions=1, min_n_datapoints_a_b
                 if point >= (datapoints.size - 1):  # if end of the data points leave the for-loop
                     break
                 # close a bin if there are at least min_n_datapoints_a_bin and the next value is bigger
-                if datapoints[last_index:point + 1].size >= min_n_datapoints_a_bin and datapoints[point] < datapoints[
-                    point + 1]:
+                if datapoints[last_index:point + 1].size >= min_n_datapoints_a_bin and datapoints[point] < \
+                        datapoints[
+                            point + 1]:
                     list_points_of_support.append(datapoints[point + 1])
                     last_index = point + 1
             if len(list_points_of_support) > 0:  # test that there is at least one point of support (it can be if there are only constant value up to the first ones which are less than min_n_datapoints_a_bin
@@ -95,27 +97,30 @@ def get_mutual_information(data, number_output_functions=1, min_n_datapoints_a_b
                 freq_data[id_output]) < 2:  # Warn if the output function is constant e.g. due to an unsuitable binning
             print("Warning: Output function " + str(id_output) + " is constant!")
 
-
     # Calculate the mutual information for each feature with the corresponding component of the output function
     list_of_data_frames = []
-    mutual_info = np.ones((1,len(left_features) - number_output_functions + 1))  # number of featuers plus one component of the output-function
+    mutual_info = np.ones((1,
+                           len(left_features) - number_output_functions + 1))  # number of featuers plus one component of the output-function
 
-
-# N times for loop here to shuffle N times and store the sum_MI
+    # N times for loop here to shuffle N times and store the sum_MI
     if perm_test_flag:
-        N=N+1
+        N = N + 1
 
     total_MI_for_each_permutation = []
     for cnt in range(N):
+<<<<<<< HEAD
         t_start=time.time()
         sum_list =[]
+=======
+        sum_list = []
+>>>>>>> 93fe218 (vectorized implementation of MI)
         for i in range(0, number_output_functions):
-            basis_log_mutual_information = len(freq_data[i])
+            basis_log_mutual_information = len(freq_data[i])  # len(discretization of target)
             # shuffle (data [i,:] ) ---> data[i,:]
 
-            if perm_test_flag and cnt>0:
-                perm_labels = permute_1d_array(data [i,:].copy())
-                data[i,:] = perm_labels
+            if perm_test_flag and cnt > 0:
+                perm_labels = permute_1d_array(data[i, :].copy())
+                data[i, :] = perm_labels
 
             list_of_features = list(range(number_output_functions, len(left_features)))
             list_of_features.insert(0, i)
@@ -123,9 +128,10 @@ def get_mutual_information(data, number_output_functions=1, min_n_datapoints_a_b
 
             for j in list_of_features:
                 freq_data_product = ((
-                np.histogram2d(data[i, :], data[left_features[j], :], bins=(l[i], l[left_features[j]]))[0])) / n
+                    np.histogram2d(data[i, :], data[left_features[j], :], bins=(l[i], l[left_features[j]]))[0])) / n
                 expfreq = (np.outer(freq_data[i], freq_data[left_features[j]])) / (n * n)
                 if j < number_output_functions:
+<<<<<<< HEAD
                     if(len(freq_data[i])>1):
                         mutual_info[0, 0] = np.sum(np.array(list(
                             map(make_summand_from_frequencies, freq_data_product.flatten().tolist(),
@@ -139,25 +145,181 @@ def get_mutual_information(data, number_output_functions=1, min_n_datapoints_a_b
                                 expfreq.flatten().tolist()))))
                     else:
                         mutual_info[0, j - number_output_functions + 1] = 0
+=======
+>>>>>>> 93fe218 (vectorized implementation of MI)
 
-            sum_mi = np.sum(mutual_info[0,1:]) # the sum over all features for each output
+                    mutual_info[0, 0]  = compute_normalized_mutual_information_np(freq_data_product, expfreq, basis_log_mutual_information)
+
+                else:
+                    mutual_info[0, j - number_output_functions + 1] = compute_normalized_mutual_information_np(freq_data_product, expfreq, basis_log_mutual_information)
+
+            sum_mi = np.sum(mutual_info[0, 1:])  # the sum over all features for each output
             sum_list.append(sum_mi)
-            pd_mutual_information = pd.DataFrame({"index feature": id_features, "mutual information": mutual_info.tolist()[0]})
+            pd_mutual_information = pd.DataFrame(
+                {"index feature": id_features, "mutual information": mutual_info.tolist()[0]})
             pd_mutual_information['index feature'] = pd_mutual_information['index feature'].astype(int)
 
             list_of_data_frames.append(pd_mutual_information)
 
-        if cnt==0:
-            actual_total_MI = sum(sum_list) # sum over all outputs (previously done on the features)
+        if cnt == 0:
+            actual_total_MI = sum(sum_list)  # sum over all outputs (previously done on the features)
             actual_list_of_df = list_of_data_frames  # we can return this instead of None
-        else: # permutation test ---> values come to the list to make a distribution:
+        else:  # permutation test ---> values come to the list to make a distribution:
             total_MI_for_each_permutation.append(sum(sum_list))
+<<<<<<< HEAD
         t_end=time.time()
         print("cnt:", cnt, "Took:",t_end-t_start)
 
     if perm_test_flag==False:
         return list_of_data_frames, actual_total_MI,None   ,     None       ,         None
+=======
+
+    if perm_test_flag == False:
+        return list_of_data_frames, actual_total_MI, None, None, None
+>>>>>>> 93fe218 (vectorized implementation of MI)
     else:
         avg_MI_permute = sum(total_MI_for_each_permutation) / len(total_MI_for_each_permutation)
-        pvalue = np.sum(np.array(total_MI_for_each_permutation) > actual_total_MI)/len(total_MI_for_each_permutation)
-        return None,                actual_total_MI, pvalue, avg_MI_permute  ,total_MI_for_each_permutation
+        pvalue = np.sum(np.array(total_MI_for_each_permutation) > actual_total_MI) / len(
+            total_MI_for_each_permutation)
+        return None, actual_total_MI, pvalue, avg_MI_permute, total_MI_for_each_permutation
+# def get_mutual_information_old(data, number_output_functions=1, min_n_datapoints_a_bin = None, perm_test_flag=True, N=10):
+#
+#     """
+#         data: a 2D numpy array in which the first "number_output_functions" rows are the values of the output function (or target values) and the other rows are the values of the features (input or context)
+#         Implementation of MI code
+#         samples : each sample stored in one column
+#         n_col---> number of samples
+#         n_rows ---> number of output + features (input)
+#         i.e
+#         m = data.shape[0]  # number features (plus targets)
+#         n = data.shape[1]  # number of data points
+#         the whole length of target + context = m ---> target comes first
+#         each column of the data: [t1, t2, ..., t_num_output_function, ctx1, ctx2, ... m ]
+#         number_output_functions: how many outputs we have
+#     """
+#     # Calulate the Shannon mutual information
+#     def make_summand_from_frequencies(x, y):
+#         if x == 0:
+#             return 0
+#         else:
+#             return x * math.log2(x / y) / math.log2(basis_log_mutual_information)
+#
+#
+#     m = data.shape[0]
+#     n = data.shape[1]
+#     l = [0 for i in range(0, m)]
+#     freq_data = [0 for i in range(0, m)]
+#     left_features = [i for i in range(0, m)]
+#     list_variables = [i for i in range(0, m)]
+#     data = data[list_variables, :]
+#
+#     if (min_n_datapoints_a_bin is None):
+#         min_n_datapoints_a_bin = 0.05*n
+#
+#
+#     constant_features = []
+#
+#     for i in range(0, m):
+#         mindata = min(data[i, :])
+#         maxdata = max(data[i, :])
+#         if maxdata <= mindata:
+#             print("Feature #"f"{list_variables[i]}" " has only constant values")
+#             left_features.remove(i)
+#             constant_features.append(list_variables[i])
+#         else:
+#             # start the binning by sorting the data points
+#             list_points_of_support = []
+#             datapoints = data[i, :].copy()
+#             datapoints.sort()
+#             last_index = 0
+#             # go through the data points and bin them
+#             for point in range(0, datapoints.size):
+#                 if point >= (datapoints.size - 1):  # if end of the data points leave the for-loop
+#                     break
+#                 # close a bin if there are at least min_n_datapoints_a_bin and the next value is bigger
+#                 if datapoints[last_index:point + 1].size >= min_n_datapoints_a_bin and datapoints[point] < datapoints[
+#                     point + 1]:
+#                     list_points_of_support.append(datapoints[point + 1])
+#                     last_index = point + 1
+#             if len(list_points_of_support) > 0:  # test that there is at least one point of support (it can be if there are only constant value up to the first ones which are less than min_n_datapoints_a_bin
+#                 if list_points_of_support[0] > datapoints[
+#                     0]:  # add the first value as a point of support if it does not exist (less than min_n_datapoints_a_bin at the beginning)
+#                     list_points_of_support.insert(0, datapoints[0])
+#             else:
+#                 list_points_of_support.append(datapoints[0])
+#             list_points_of_support.append(datapoints[
+#                                               -1] + 0.1)  # Add last point of support such that last data point is included (half open interals in Python!)
+#             if datapoints[datapoints >= list_points_of_support[
+#                 -2]].size < min_n_datapoints_a_bin:  # if last bin has not at least min_n_datapoints_a_bin fuse it with the one before the last bin
+#                 if len(list_points_of_support) > 2:  # Test if there are at least 3 points of support (only two can happen if there only constant values at the beginning and only less than n_min_datapoints_a_bin in the end)
+#                     list_points_of_support.pop(-2)
+#             l[i] = list_points_of_support
+#             freq_data[i] = np.histogram(data[i, :], bins=l[i])[0]
+#
+#     # Check for constant features
+#     if constant_features != []:
+#         print("List of features with constant values:")
+#         print(constant_features)
+#     for id_output in range(0, number_output_functions):
+#         if id_output in constant_features or len(
+#                 freq_data[id_output]) < 2:  # Warn if the output function is constant e.g. due to an unsuitable binning
+#             print("Warning: Output function " + str(id_output) + " is constant!")
+#
+#
+#     # Calculate the mutual information for each feature with the corresponding component of the output function
+#     list_of_data_frames = []
+#     mutual_info = np.ones((1,len(left_features) - number_output_functions + 1))  # number of featuers plus one component of the output-function
+#
+#
+# # N times for loop here to shuffle N times and store the sum_MI
+#     if perm_test_flag:
+#         N=N+1
+#
+#     total_MI_for_each_permutation = []
+#     for cnt in range(N):
+#         sum_list =[]
+#         for i in range(0, number_output_functions):
+#             basis_log_mutual_information = len(freq_data[i])
+#             # shuffle (data [i,:] ) ---> data[i,:]
+#
+#             if perm_test_flag and cnt>0:
+#                 perm_labels = permute_1d_array(data [i,:].copy())
+#                 data[i,:] = perm_labels
+#
+#             list_of_features = list(range(number_output_functions, len(left_features)))
+#             list_of_features.insert(0, i)
+#             id_features = np.array(list_variables)[list_of_features]
+#
+#             for j in list_of_features:
+#                 freq_data_product = ((
+#                 np.histogram2d(data[i, :], data[left_features[j], :], bins=(l[i], l[left_features[j]]))[0])) / n
+#                 expfreq = (np.outer(freq_data[i], freq_data[left_features[j]])) / (n * n)
+#                 if j < number_output_functions:
+#                     mutual_info[0, 0] = np.sum(np.array(list(
+#                         map(make_summand_from_frequencies, freq_data_product.flatten().tolist(),
+#                             expfreq.flatten().tolist()))))
+#                 else:
+#                     mutual_info[0, j - number_output_functions + 1] = np.sum(np.array(list(
+#                         map(make_summand_from_frequencies, freq_data_product.flatten().tolist(),
+#                             expfreq.flatten().tolist()))))
+#
+#             sum_mi = np.sum(mutual_info[0,1:]) # the sum over all features for each output
+#             sum_list.append(sum_mi)
+#             pd_mutual_information = pd.DataFrame({"index feature": id_features, "mutual information": mutual_info.tolist()[0]})
+#             pd_mutual_information['index feature'] = pd_mutual_information['index feature'].astype(int)
+#
+#             list_of_data_frames.append(pd_mutual_information)
+#
+#         if cnt==0:
+#             actual_total_MI = sum(sum_list) # sum over all outputs (previously done on the features)
+#             actual_list_of_df = list_of_data_frames  # we can return this instead of None
+#         else: # permutation test ---> values come to the list to make a distribution:
+#             total_MI_for_each_permutation.append(sum(sum_list))
+#
+#
+#     if perm_test_flag==False:
+#         return list_of_data_frames, actual_total_MI,None   ,     None       ,         None
+#     else:
+#         avg_MI_permute = sum(total_MI_for_each_permutation) / len(total_MI_for_each_permutation)
+#         pvalue = np.sum(np.array(total_MI_for_each_permutation) > actual_total_MI)/len(total_MI_for_each_permutation)
+#         return None,                actual_total_MI, pvalue, avg_MI_permute  ,total_MI_for_each_permutation
--- example.py
+++ example.py
@@ -44,11 +44,15 @@ def convert_to_numpy(data):
     else:
         raise ValueError("Input data must be either a NumPy array or a PyTorch tensor")
 
-
+# please modify the below config including model architecture input/contex size and target/prediction length and number of epcohs ....
 
 config = """
 learn:
+<<<<<<< HEAD
   epochs: 40
+=======
+  epochs: 10
+>>>>>>> 93fe218 (vectorized implementation of MI)
   batch_size: 450
   lr: 0.0001
   save_model: True
@@ -69,7 +73,7 @@ transformer_arch:
 
 data_reader:
   context_size: 49
-  pred_len: 3
+  pred_len: 2
 """
 
 cfg = yaml.safe_load(config)
@@ -141,9 +145,9 @@ if not(os.path.exists(save_dir)):
     save_path = os.path.join(save_dir,"model.ckpt")
 
 
-parallel_net = m2
+predictor_net = m2 # select any of the defined models.
 
-transformer_learn = transformer_trainer.Learn(parallel_net, loss=cfg["learn"]["loss"], config=cfg  )
+transformer_learn = transformer_trainer.Learn(predictor_net, loss=cfg["learn"]["loss"], config=cfg)
 
 
 ############ statistical tests before training
@@ -156,9 +160,14 @@ print("=========================  initial Chi-square test & train  =============
 results0, pvl0, cnt_dep0,_,_ = chisquare_test(input_to_predictability_measures_test_data ,number_output_functions= cfg["data_reader"]["pred_len"],  bonfer=True)
 results1, pvl1,cnt_dep1,_,_ = chisquare_test(input_to_predictability_measures_train_data,number_output_functions= cfg["data_reader"]["pred_len"],  bonfer=True)
 
+# print("Initial chisquare on test set shows dependency on the following variables ", results0)
+# print('Number of dependent variables on test set (pairwise):', cnt_dep0)
+
+
 print("=========================  initial MI test & train  ===================================")
 
 ############
+<<<<<<< HEAD
 _, SUM_MI_initial_test, init_MI_pv_test, avg_MI_initial_test_permute, SUM_MI_initial_test_permuted = get_mutual_information(input_to_predictability_measures_test_data, number_output_functions=cfg["data_reader"]["pred_len"], perm_test_flag=True, N=300)
 print('SUM_MI_initial_test:', SUM_MI_initial_test)
 print("SUM_MI_initial_test_permuted", SUM_MI_initial_test_permuted)
@@ -169,6 +178,18 @@ _, SUM_MI_initial_train, init_MI_pv_train, avg_MI_initial_train_permute, SUM_MI_
 print('SUM_MI_initial_test:', SUM_MI_initial_test)
 print("SUM_MI_initial_test_permuted", SUM_MI_initial_test_permuted)
 print("initial MI is less than", init_MI_pv_test ,"% of the MI in a random permutations")
+=======
+_, SUM_MI_initial_test, init_MI_pv_test, avg_MI_initial_test_permute, SUM_MI_initial_test_permuted = get_mutual_information(input_to_predictability_measures_test_data, number_output_functions=cfg["data_reader"]["pred_len"], perm_test_flag=True, N=100)
+print('SUM_Pairwise_MI_initial_test:', SUM_MI_initial_test)
+print("SUM_Pairwise_MI_initial_test_permuted", SUM_MI_initial_test_permuted)
+print("Initial MI is less than", init_MI_pv_test ,"% of the MI in a random permutations")
+
+
+_, SUM_MI_initial_train, init_MI_pv_train, avg_MI_initial_train_permute, SUM_MI_initial_train_permuted = get_mutual_information(input_to_predictability_measures_train_data, number_output_functions=cfg["data_reader"]["pred_len"], perm_test_flag=True, N=100)
+print('SUM_MI_initial_train:', SUM_MI_initial_train)
+print("SUM_MI_initial_train_permuted", SUM_MI_initial_train_permuted)
+print("initial MI is less than", init_MI_pv_train ,"% of the MI in a random permutations")
+>>>>>>> 93fe218 (vectorized implementation of MI)
 ############
 
 print("=========================  initial pearson_test  ===================================")
@@ -185,39 +206,42 @@ if cfg["learn"]["load"] == False:
     #### Train the Model
     transformer_learn.train(train_batched, cfg["learn"]["epochs"], cfg["learn"]["batch_size"], test_batched)
 
-parallel_net.load_state_dict(torch.load(cfg["learn"]["save_path"]))
+predictor_net.load_state_dict(torch.load(cfg["learn"]["save_path"]))
 print('>>>>>>>>>>Loaded The Best Model From Local Folder<<<<<<<<<<<<<<<<<<<')
+print('>>>>>>>>>>inference started<<<<<<<<<<<<<<<<<<<')
 
 
-#inference
+#inference started
+
+# this inference object can be used for any model/network
+transformer_infer = transformer_inference.Infer(predictor_net, config=cfg)
 
 
-transformer_infer = transformer_inference.Infer(parallel_net, config=cfg)
-
-#k = cfg.data_reader.context_size #=context_size=75
-
-
-print(" Test started......")
 result={}
 # the last _ is residual list
 pred_mean, _ , gt_multi, observed_part_te, residual_target_te = transformer_infer.predict_mbrl(test_batched, batch_size=cfg["learn"]["batch_size"] )  # returns normalized predicted packets
-print("observed_part_test.shape:", observed_part_te.shape)
-print("residual_target_test.shape:", residual_target_te.shape)
+# print("observed_part_test.shape:", observed_part_te.shape)
+# print("residual_target_test.shape:", residual_target_te.shape)
 
 residual_ctx_and_tar_te = torch.cat([observed_part_te, residual_target_te], dim=1)
-print("residual_ctx_and_tar_te:", residual_ctx_and_tar_te.shape)
+# print("residual_ctx_and_tar_te:", residual_ctx_and_tar_te.shape)
 
 
 
 
 pred_mean_tr, _, gt_multi_tr, observed_part_tr, residual_target_tr =  transformer_infer.predict_mbrl(train_batched,batch_size=cfg["learn"]["batch_size"])  # returns normalized predicted packets
 residual_ctx_and_tar_tr = torch.cat([observed_part_tr, residual_target_tr], dim=1)
-print("residual_ctx_and_tar_tr.shape:", residual_ctx_and_tar_tr.shape)
-print("check and plot the stuff")
+# print("residual_ctx_and_tar_tr.shape:", residual_ctx_and_tar_tr.shape)
+# print("check and plot the stuff")
 
 
+
+# preparing input to measure how much determinitic relations left
 input_to_predictability_measures_test_data  = circular_shift_features( convert_to_numpy(residual_ctx_and_tar_te).squeeze().swapaxes(0, 1), t=cfg["data_reader"]["pred_len"])
 input_to_predictability_measures_train_data  = circular_shift_features( convert_to_numpy(residual_ctx_and_tar_tr).squeeze().swapaxes(0, 1), t=cfg["data_reader"]["pred_len"])
+
+
+
 
 
 print(" ======================chi-square on residual_test:=============================")
@@ -226,17 +250,22 @@ results2, pvl2 ,cnt_dep2,_,_ = chisquare_test(input_to_predictability_measures_t
 results3, pvl3, cnt_dep3,_,_ = chisquare_test(input_to_predictability_measures_train_data,number_output_functions= cfg["data_reader"]["pred_len"], bonfer=True)
 
 
-print("=========================MI and permutation on residual_test ============================")
+print("=========================MI and permutation-test on residual_test ============================")
 
 
 _, sum_res_test ,  res_MI_pv_test,   avg_MI_res_test_permute, sum_res_test_permuted = get_mutual_information(input_to_predictability_measures_test_data, number_output_functions=cfg["data_reader"]["pred_len"], perm_test_flag=True, N=300)
 _, sum_res_train , res_MI_pv_train, avg_MI_res_train_permute, sum_res_train_permuted = get_mutual_information(input_to_predictability_measures_train_data, number_output_functions=cfg["data_reader"]["pred_len"], perm_test_flag=True, N=300)
+
+print('Remaining MI on test set:', sum_res_test)
+
 
 print("==================================== Pearson on test_res ===================================")
 #input_to_pearson=circular_shift_features(residual_ctx_and_tar_te.squeeze().swapaxes(0, 1),t=cfg["learn"]["pred_len"])
 sum_r_test_res = pearson_test(input_to_predictability_measures_test_data,number_output_functions=cfg["data_reader"]["pred_len"])
 sum_r_train_res = pearson_test(input_to_predictability_measures_train_data,number_output_functions=cfg["data_reader"]["pred_len"])
 
+print("============================================================================================")
 
+print("MI on test-set decreased from {:.5f} to {:.5f}".format(SUM_MI_initial_test,sum_res_test))
 
 
